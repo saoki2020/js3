f48587fca02b5cdbaf187467dfa1a48d216d7313
saoki2020 <md.mendelevium101@gmail.com>
2021-10-20 17:28:49 +0900

    JavaScript課題3-1 3回目

--- js/js3.js
+++ js/js3.js
@@ -2,46 +2,56 @@
 {
   const tasks = [];
 
+  const showTask = () => {
+    const tbody = document.querySelector('tbody');
+
+    //clear screen
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+
+    tasks.forEach(task => {
+      const row = tbody.insertRow();
+
+      const idCell = row.insertCell();
+      const id = document.createTextNode(task.id);
+      idCell.appendChild(id);
+
+      const commentCell = row.insertCell();
+      const comment = document.createTextNode(task.comment);
+      commentCell.appendChild(comment);
+
+      const statusCell = row.insertCell();
+      const statusBtn = document.createElement("button");
+      statusBtn.textContent = task.status;
+      statusCell.appendChild(statusBtn);
+
+      const deleteCell = row.insertCell();
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "削除";
+      deleteCell.appendChild(deleteBtn);
+
+     //delete task
+        deleteBtn.onclick = () => {
+          tasks.splice(task.id, 1);
+          tasks.forEach((task, index) => {
+            task.id = index;
+          });
+          showTask();
+        }
+    });
+  }
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
 
     //save task
-    const task = document.getElementById('input_task');
-    tasks.push({id: tasks.length, comment: task.value, status: "作業中"});
+    const inputTask = document.getElementById('input_task');
+    tasks.push({id: tasks.length, comment: inputTask.value, status: "作業中"});
 
-    const tbody = document.querySelector('tbody');
+    showTask();
 
-    //output on screen
-    const show_task = () => {
-      //clear screen
-      while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-      }
-
-      tasks.forEach(task => {
-        const row = tbody.insertRow();
-
-        const idCell = row.insertCell();
-        const id = document.createTextNode(task.id);
-        idCell.appendChild(id);
-
-        const commentCell = row.insertCell();
-        const comment = document.createTextNode(task.comment);
-        commentCell.appendChild(comment);
-
-        const statusCell = row.insertCell();
-        const statusBtn = document.createElement("BUTTON");
-        statusBtn.textContent = task.status;
-        statusCell.appendChild(statusBtn);
-
-        const deleteCell = row.insertCell();
-        const deleteBtn = document.createElement("BUTTON");
-        deleteBtn.textContent = "削除";
-        deleteCell.appendChild(deleteBtn);
-      });
-      //clear form
-      task.value = "";
-    }
-    show_task();
+    //clear form
+    inputTask.value = "";
   });
+
 }
